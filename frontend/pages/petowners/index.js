e1b5d939cb0d9c0a36f99db7f9753f2bb2b46a93
Camilo Saez <camjasaez@gmail.com>
2023-01-11 20:29:40 -0300

    arreglando imports

--- frontend/pages/petowners/index.js
+++ frontend/pages/petowners/index.js
@@ -17,6 +17,7 @@ import {
 import { getPetOwner } from '../../utils/getPetOwnerData';
 import { deletePetOwner } from '../../utils/getPetOwnerData';
 import { useAuth } from '../../components/Auth';
+import { useEffect } from 'react';
 
 function PetOwner({ petowner }) {
   const { checkAuth } = useAuth();
